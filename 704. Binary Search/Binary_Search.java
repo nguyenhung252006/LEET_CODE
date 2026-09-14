4b7ec94d505bbc06cee94d53cefd2efb944af051
nguyenhung252006 <nguyenviethungk56@gmail.com>
2026-09-14 22:39:57 +0700

    704. Binary Search

---
+++ 704. Binary Search/Binary_Search.java
@@ -0,0 +1,24 @@
+public class Binary_Search {
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -1, 0, 5 };
+        System.out.print(search(nums, 5));
+    }
+
+}
